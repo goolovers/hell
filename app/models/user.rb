7ceea3a6b87ffb21f75058ccc765913a9b7ff667
goolovers <goolovers@sina.com>
2019-10-17 17:32:54 +0800

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 # 用户管理类，user_type:0->超级管理员;1->管理用户；2->普通用户
 class User < ApplicationRecord
+
+	validates :user_code, presence: {message:'不可为空！'}
+	validates :user_name, presence: {message:'不可为空！'}
+	validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "只能包含数字或字母！" }
 end
